7e5c2ccc61c6dc74ffecdee036c0b8b47aa58ea6
SND\XVincentX_cp <SND\XVincentX_cp@91bfc952-fec6-4be4-b192-9eb3b8389945>
2013-09-09 20:12:03 +0000

    Added C# binding project
    git-svn-id: https://open103.svn.codeplex.com/svn@30756 91bfc952-fec6-4be4-b192-9eb3b8389945

---
+++ DotNetOpen103/DotNetOpen103.h
@@ -0,0 +1,17 @@
+// DotNetOpen103.h
+
+#pragma once
+
+using namespace System;
+
+namespace DotNetOpen103 {
+
+	public ref class IEC8705103Manager
+	{
+		IEC8705103Manager()
+		{
+				
+		}
+
+	};
+}
